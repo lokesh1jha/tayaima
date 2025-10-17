a01a331eea5f206702d06f3ffe535d0f7a0c0142
Lokesh Kumar Jha <lokesh1jha@gmail.com>
2025-10-17 15:41:29 +0530

    mig

--- prisma/migrations/20250116000000_add_phone_authentication/migration.sql
+++ prisma/migrations/20250116000000_add_phone_authentication/migration.sql
@@ -12,8 +12,6 @@ CREATE TABLE "OtpVerification" (
 );
 
 -- AlterTable
-ALTER TABLE "User" ADD COLUMN "phone" TEXT;
-ALTER TABLE "User" ADD COLUMN "phoneVerified" TIMESTAMP(3);
 ALTER TABLE "User" ALTER COLUMN "email" DROP NOT NULL;
 
 -- CreateIndex
